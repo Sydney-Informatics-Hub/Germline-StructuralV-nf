bb5684188b494f664de4b669024fa0ec04bab951
georgiesamaha <georgina.samaha@sydney.edu.au>
2023-04-28 09:58:21 +1000

    change tiddit version in pull script

--- scripts/pullContainers.sh
+++ scripts/pullContainers.sh
@@ -22,6 +22,6 @@ singularity pull docker://quay.io/biocontainers/bcftools:1.15.1--hfe4b78e_1
 singularity pull docker://quay.io/biocontainers/mulled-v2-40295ae41112676b05b649e513fe7000675e9b84:0b4be2c719f99f44df34be7b447b287bb7f86e01-0 
 singularity pull docker://brentp/smoove:v0.2.7
 singularity pull docker://quay.io/biocontainers/survivor:1.0.7--hd03093a_2
-singularity pull docker://sydneyinformaticshub/tiddit:3.5.2
+singularity pull docker://sydneyinformaticshub/tiddit:3.6.0
 singularity pull docker://sydneyinformaticshub/annotsv:3.2.1
 singularity pull docker://quay.io/biocontainers/manta:1.6.0--h9ee0642_2
