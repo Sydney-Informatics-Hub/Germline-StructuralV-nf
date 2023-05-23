d6aa30cfe35c45a4872052dd29b9c99d47d83ebd
georgiesamaha <georgina.samaha@sydney.edu.au>
2023-05-23 17:22:24 +1000

    fix manta container, fix pbs script

--- scripts/PBS_gadi_runpipeline.sh
+++ scripts/PBS_gadi_runpipeline.sh
@@ -16,14 +16,13 @@
 # See: https://opus.nci.org.au/display/DAE/Nextflow
 # See: https://opus.nci.org.au/display/Help/Singularity
 module load singularity
-module load nextflow/23.04.1
-module load java
+module load nextflow
 
 # Fill in these variables for your run
 ref= #full path to your reference.fasta
-samples= #platinumgenomes_1.tsv #full path to your input.tsv file
+samples= #full path to your input.tsv file
 annotsvDir= #full path to directory housing Annotations_Human directory
-annotsvMode= #see annotation mode in https://github.com/lgmgeo/AnnotSV/blob/master/README.AnnotSV_3.3.4.pdf
+annotsvMode= #both|full|split. see annotation mode in https://github.com/lgmgeo/AnnotSV/blob/master/README.AnnotSV_3.3.4.pdf
 outDir= #full path for results directory
 
 # set singularity cache dir
